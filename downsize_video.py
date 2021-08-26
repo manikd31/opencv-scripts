76b6ab3cda57dbc42d26a6367f776accc3072256
manikd31 <manik.dhingra311@gmail.com>
2021-08-26 16:20:52 -0400

    Initial commit

---
+++ downsize_video.py
@@ -0,0 +1,146 @@
+"""
+Script to downsize a video, i.e. reduce the fps of any video with given fps value.
+
+Usage:
+    downsize_video.py [--path_in=PATH_IN]
+                      [--path_out=PATH_OUT]
+                      [--file_name=FILE_NAME]
+                      [--target_fps=TARGET_FPS]
+                      [--is_dir]
+    downsize_video.py (-h | --help)
+
+Options:
+    --path_in=PATH_IN           Path to the folder containing the video file
+    --path_out=PATH_OUT         Path to save the downsized video file
+    --file_name=FILE_NAME       Name of the video (with extension)
+    --target_fps=TARGET_FPS     Target FPS
+    --is_dir                    Boolean to indicate if the path is for video or complete dataset
+"""
+
+import os
+import cv2
+from docopt import docopt
+
+TEST_PATH_IN = r"C:/Users/Manik/Desktop/test_CV_videos"
+TEST_PATH_OUT = TEST_PATH_IN
+TEST_FILE_NAME = "test_video0.mp4"
+TEST_TARGET_FPS = 4.0
+
+ID_WIDTH = 3
+ID_HEIGHT = 4
+ID_FPS = 5
+ID_FRAME_COUNT = 7
+
+FOURCC = 0x7634706d
+
+
+def parse_video(cap, path_out, file_name, target_fps):
+    """
+    Helper method to get VideoCapture properties for the given video.
+
+    :param cap:
+        VideoCapture object for the video
+    :param path_out:
+        Path to save the downsized video
+    :param file_name:
+        Name of the video file to downsize
+    :param target_fps:
+        Target FPS for downsizing video
+    """
+    name, ext = file_name.split('.')
+    new_file_name = f"{name}_downsized_at_fps={int(target_fps)}.{ext}"
+
+    width = int(cap.get(ID_WIDTH))
+    height = int(cap.get(ID_HEIGHT))
+    current_fps = int(cap.get(ID_FPS))
+    frame_count = int(cap.get(ID_FRAME_COUNT))
+    # frame_jump = int(current_fps / target_fps)
+    frame_jump = current_fps / target_fps  # Use this for trying method-2
+
+    out = cv2.VideoWriter(os.path.join(path_out, new_file_name), FOURCC, target_fps, (width, height))
+
+    ################################## Method 1 ################################
+    # This part doesn't work well in case the jump is between [1, 2). It will
+    # record all frames but save at lower fps thereby increasing video length.
+    ############################################################################
+    # i = 0
+    # while i < frame_count:
+    #     if i % frame_jump == 0:
+    #         cap.set(1, i)
+    #         ret, frame = cap.read()
+    #         out.write(frame)
+    #     i += 1
+
+    ################################# Method 2 #################################
+    # Works!!
+    ############################################################################
+    i = 0
+    prev_frame = -1
+    while i < frame_count:
+        new_frame = int(i * frame_jump)
+        if new_frame != prev_frame:
+            cap.set(1, new_frame)
+            ret, frame = cap.read()
+            out.write(frame)
+            prev_frame = new_frame
+        i += 1
+
+    ################################## Method 3 ################################
+    # This code just saves the video at target_fps, but doesn't drop any frames.
+    # So the final video is longer than the original.
+    ############################################################################
+    # while cap.isOpened():
+    #     ret, frame = cap.read()
+    #     if ret:
+    #         out.write(frame)
+    #     else:
+    #         break
+
+    out.release()
+
+
+def main():
+    """Main body of the script to be run."""
+
+    # Parse arguments
+    args = docopt(__doc__)
+    path_in = args["--path_in"] or TEST_PATH_IN
+    path_out = args["--path_out"] or TEST_PATH_OUT
+    file_name = args["--file_name"] or TEST_FILE_NAME
+    target_fps = float(args["--target_fps"]) if args["--target_fps"] else TEST_TARGET_FPS
+    is_dir = args["--is_dir"] or False
+
+    if not os.path.isdir(path_in):
+        print(f"    [ERROR]\tThe folder \"{path_in}\" doesn't exist.")
+    elif not os.path.isfile(os.path.join(path_in, file_name)):
+        print(f"    [ERROR]\tThe file \"{file_name}\" or folder \"{path_in}\" doesn't exist.")
+    else:
+        if is_dir:
+            all_videos = os.listdir(path_in)
+            num_videos = len(all_videos)
+            print(f"    [INFO]\tFound {num_videos} videos.")
+            for _id, video in enumerate(all_videos):
+                video_path = os.path.join(path_in, video)
+                capture = cv2.VideoCapture(video_path)
+                message = ""
+                if target_fps >= capture.get(ID_FPS):
+                    print(f"    [INFO]\tTarget FPS is equal to or larger than source FPS, skipping video {_id + 1}.")
+                else:
+                    os.makedirs(path_out, exist_ok=True)
+                    parse_video(capture, path_out, video, target_fps)
+                    print(f"    [INFO]\tProcessed {_id + 1} / {num_videos} videos.")
+                capture.release()
+            print("    [INFO]\tDone!")
+        else:
+            video_path = os.path.join(path_in, file_name)
+            capture = cv2.VideoCapture(video_path)
+            if target_fps >= capture.get(ID_FPS):
+                print(f"    [INFO]\tTarget FPS is equal to or larger than source FPS, skipping video.")
+            else:
+                parse_video(capture, path_out, file_name, target_fps)
+                print("    [INFO]\tDone!")
+            capture.release()
+
+
+if __name__ == "__main__":
+    main()
---
+++ record_video.py
@@ -0,0 +1,157 @@
+"""
+Demo script for openCV tasks.
+
+Usage:
+    record_video.py
+    record_video.py (-h | --help)
+"""
+
+import cv2
+import os
+from PyInquirer import prompt
+import time
+
+path_out = r'C:\Users\Manik\Desktop\test_CV_videos'
+
+FPS16 = 16.0
+FPS24 = 24.0
+FPS30 = 30.0
+
+STD_DIMENSIONS = {
+    "480p": (640, 480),
+    "720p": (1280, 720),
+    "1080p": (1920, 1080),
+}
+
+VIDEO_TYPE = [
+    'mp4',
+    'avi'
+]
+
+
+def change_res(cap, width, height):
+    """
+    Helper method to set the video resolution for the current VideoCapture object.
+
+    :param cap:
+        The current VideoCapture source object
+    :param width:
+        Desired width of the output video
+    :param height:
+        Desired height of the output video
+    """
+
+    cap.set(3, width)
+    cap.set(4, height)
+
+
+def get_dims(cap, res='480p'):
+    """
+    Helper method to get the appropriate dimensions of the video based on desired resolution.
+
+    :param cap:
+        VideoCapture source object
+    :param res:
+        Desired resolution, one of ('480p', '720p', '1080p')
+    :return:
+        The desired width and height for the output video
+    """
+
+    width, height = STD_DIMENSIONS['480p']
+    if res in STD_DIMENSIONS:
+        width, height = STD_DIMENSIONS[res]
+    change_res(cap, width, height)
+    return width, height
+
+
+def find_closest_fps(_fps):
+    """
+    Helper method to find the closest FPS value to the recorded FPS.
+
+    :param _fps:
+        Recorded FPS
+    :return:
+        Closes FPS to save video file
+    """
+
+    if _fps <= FPS24:
+        video_fps = FPS24 if abs(FPS24 - _fps) <= abs(FPS16 - _fps) else FPS16
+    else:
+        video_fps = FPS24 if abs(FPS24 - _fps) <= abs(FPS30 - _fps) else FPS30
+
+    return video_fps
+
+
+def main():
+    """Main body of the script to be run."""
+
+    answer_video_res = prompt({
+        'type': 'list',
+        'name': 'video_res',
+        'message': 'Select the video resolution',
+        'choices': [
+            '480p',
+            '720p'
+        ],
+    })
+    video_res = answer_video_res['video_res']
+
+    answer_video_name = prompt({
+        'type': 'input',
+        'name': 'video_name',
+        'message': 'Enter the video name: ',
+        'default': 'video.mp4'
+    })
+    video_name = answer_video_name['video_name']
+
+    if len(video_name.split('.')) < 2 or video_name.split('.')[1] not in VIDEO_TYPE:
+        answer_video_ext = prompt({
+            'type': 'list',
+            'name': 'video_ext',
+            'message': 'Select the video extension',
+            'choices': [
+                '.mp4',
+                '.avi'
+            ],
+        })
+        video_name += answer_video_ext['video_ext']
+
+    capture = cv2.VideoCapture(0)
+    dims = get_dims(capture, video_res)
+
+    frames = []
+    s_time = time.time()
+    p_time = s_time
+
+    os.makedirs(path_out, exist_ok=True)
+    while True:
+        _, frame = capture.read()
+        frame = cv2.flip(frame, 1)
+        frames.append(frame.copy())
+        c_time = time.time()
+
+        cv2.putText(frame, f"fps: {int(1 / (c_time - s_time))}", (20, 40), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+        s_time = c_time
+        cv2.imshow("Video", frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    f_time = time.time()
+
+    _fps = round(len(frames) / int(f_time - p_time))
+    fps = find_closest_fps(_fps)
+
+    out = cv2.VideoWriter(os.path.join(path_out, video_name), 0x7634706d, fps, dims)
+    print(f"    [INFO]\tSaving file at {os.path.join(path_out, video_name)}")
+
+    for frame in frames:
+        out.write(frame)
+
+    cv2.destroyAllWindows()
+    capture.release()
+    out.release()
+
+
+if __name__ == "__main__":
+    main()
