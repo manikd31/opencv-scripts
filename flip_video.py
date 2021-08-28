b6229f16af29ad5146d04ce14521e20286820e92
manikd31 <manik.dhingra311@gmail.com>
2021-08-27 21:40:09 -0400

    add method to flip-video

--- flip_video.py
+++ flip_video.py
@@ -13,13 +13,37 @@ TEST_PATH_IN = r"C:/Users/Manik/Desktop/test_CV_videos"
 TEST_PATH_OUT = TEST_PATH_IN
 VIDEO_EXT = ".mp4"
 FOURCC = 0x7634706d
+PROP_ID_WIDTH = 3
+PROP_ID_HEIGHT = 4
+PROP_ID_FPS = 5
 
 
-def flip():
-    print()
+def flip_video(path_in, path_out):
+    """
+    Horizontally flip the video at the given source and save at the destination.
+
+    :param path_in:
+        Path to the video to be flipped
+    :param path_out:
+        Path to save the flipped video
+    """
+
+    cap = cv2.VideoCapture(path_in)
+    out = cv2.VideoWriter(path_out, FOURCC, cap.get(PROP_ID_FPS),
+                          (int(cap.get(PROP_ID_WIDTH)), int(cap.get(PROP_ID_HEIGHT))))
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame = cv2.flip(frame, 1)
+        out.write(frame)
+    cap.release()
+    out.release()
 
 
 def main():
+    """Main body of the script to be run."""
+
     answer_videos_dir = prompt({
         'type': 'input',
         'name': 'videos_dir',
@@ -31,7 +55,7 @@ def main():
     answer_save_dir = prompt({
         'type': 'input',
         'name': 'save_dir',
-        'message': 'Enter the path to the directory to save flipped videos: ',
+        'message': 'Enter the path to save the flipped videos to: ',
         'default': TEST_PATH_OUT
     })
     save_dir = answer_save_dir['save_dir']
@@ -43,17 +67,7 @@ def main():
         video_path = os.path.join(videos_dir, video)
         flipped_name = f"{video.split('.')[0]}_flipped{VIDEO_EXT}"
         save_path = os.path.join(save_dir, flipped_name)
-
-        cap = cv2.VideoCapture(video_path)
-        out = cv2.VideoWriter(save_path, FOURCC, cap.get(5), (int(cap.get(3)), int(cap.get(4))))
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                break
-            frame = cv2.flip(frame, 1)
-            out.write(frame)
-        cap.release()
-        out.release()
+        flip_video(video_path, save_path)
 
     print("    [INFO]\tDone!")
 
