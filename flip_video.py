17fbb50352602ed407dabccfb112fcafe5c61afa
manikd31 <manik.dhingra311@gmail.com>
2021-08-27 21:25:55 -0400

    remove slow ffmpeg version

--- flip_video.py
+++ flip_video.py
@@ -5,7 +5,6 @@ Usage:
     flip_video.py (-h | --help)
 """
 
-import ffmpeg
 import os
 import cv2
 from PyInquirer import prompt
@@ -37,17 +36,6 @@ def main():
     })
     save_dir = answer_save_dir['save_dir']
 
-    answer_flip_method = prompt({
-        'type': 'list',
-        'name': 'flip_method',
-        'message': 'Select flipping method:',
-        'choices': [
-            'OpenCV',
-            'FFMPEG'
-        ]
-    })
-    flip_method = answer_flip_method['flip_method']
-
     num_videos = len(os.listdir(videos_dir))
     print(f"    [INFO]\tFound {num_videos} videos.")
     for _id, video in enumerate(os.listdir(videos_dir)):
@@ -56,23 +44,16 @@ def main():
         flipped_name = f"{video.split('.')[0]}_flipped{VIDEO_EXT}"
         save_path = os.path.join(save_dir, flipped_name)
 
-        if flip_method.lower() == 'opencv':
-            # OpenCV version -- fast
-            cap = cv2.VideoCapture(video_path)
-            out = cv2.VideoWriter(save_path, FOURCC, cap.get(5), (int(cap.get(3)), int(cap.get(4))))
-            while True:
-                ret, frame = cap.read()
-                if not ret:
-                    break
-                frame = cv2.flip(frame, 1)
-                out.write(frame)
-            cap.release()
-            out.release()
-        else:
-            # FFMPEG version -- slow
-            flipped_video = ffmpeg.hflip(ffmpeg.input(video_path))
-            out = ffmpeg.output(flipped_video, filename=save_path)
-            ffmpeg.run(out, quiet=True)
+        cap = cv2.VideoCapture(video_path)
+        out = cv2.VideoWriter(save_path, FOURCC, cap.get(5), (int(cap.get(3)), int(cap.get(4))))
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frame = cv2.flip(frame, 1)
+            out.write(frame)
+        cap.release()
+        out.release()
 
     print("    [INFO]\tDone!")
 
