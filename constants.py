9bd5615620ba4965666f8724c2e0842b7e233619
manikd31 <manik.dhingra311@gmail.com>
2021-08-30 02:45:01 -0400

    new file for constants

---
+++ constants.py
@@ -0,0 +1,41 @@
+# Directories to use as default path-in and path-out
+TEST_PATH_IN = r"C:/Users/Manik/Desktop/test_CV_videos"
+TEST_PATH_OUT = r"C:/Users/Manik/Desktop/test_CV_videos"
+TEST_FILE_NAME = "video.mp4"
+TEST_TARGET_FPS = 4
+
+# Video extension
+VIDEO_EXT = ".mp4"
+
+# VideoWriter "out" four-char code
+FOURCC = 0x7634706d
+
+# VideoCapture properties
+PROP_ID_WIDTH = 3
+PROP_ID_HEIGHT = 4
+PROP_ID_FPS = 5
+PROP_ID_FRAME_COUNT = 7
+
+# BGR codes for basic colors
+STD_COLORS = {
+    'Black': (0, 0, 0),
+    'Blue': (255, 0, 0),
+    'Cyan': (255, 255, 0),
+    'Green': (0, 255, 0),
+    'Pink': (255, 0, 255),
+    'Red': (0, 0, 255),
+    'White': (255, 255, 255),
+    'Yellow': (0, 255, 255)
+}
+
+# Standard video resolutions
+STD_DIMENSIONS = {
+    "480p": (640, 480),
+    "720p": (1280, 720),
+    "1080p": (1920, 1080),
+}
+
+# FPS values
+FPS16 = 16.0
+FPS24 = 24.0
+FPS30 = 30.0
