74b15b8ebc8b8be5d6dc451c69651fa262163e9a
manikd31 <manik.dhingra311@gmail.com>
2021-09-03 20:42:52 -0400

    new method to help process input and get predictions

---
+++ process_output.py
@@ -0,0 +1,28 @@
+import keras
+import numpy as np
+import cv2
+import os
+
+
+def process_output(model, frame: np.ndarray) -> int:
+    """
+    Process the current frame from the trained model and return output class.
+
+    :param model:
+        The saved model weights to get predictions
+    :param frame:
+        The VideoCapture frame to process
+    :return:
+        The predicted class
+    """
+
+    # Resize the frame from (280 x 280 x 3) to (28 x 28 x 1)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = cv2.flip(frame, 1)
+    frame = cv2.resize(frame, (28, 28))
+    input_frame = np.reshape(frame, (1, 28, 28, 1))
+
+    # Get prediction
+    prediction = int(np.argmax(model.predict(input_frame)))
+
+    return prediction
