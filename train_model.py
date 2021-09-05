1a5433a98486a45151659affbb4d6478c1fa315a
manikd31 <manik.dhingra311@gmail.com>
2021-09-04 20:44:19 -0400

    minor refactoring + support both dataset options

--- train_model.py
+++ train_model.py
@@ -1,4 +1,15 @@
-import keras
+"""
+Script to train a simple ConvNet on ASL or MNIST dataset.
+
+Usage:
+    train_model.py [--dataset=DATASET]
+    train_model.py (-h | --help)
+
+Options:
+    --dataset=DATASET   Select the dataset to get the trained model (MNIST or ASL)
+"""
+
+from docopt import docopt
 from keras.datasets import mnist
 from keras.layers import Conv2D
 from keras.layers import Dense
@@ -6,48 +17,115 @@ from keras.layers import Dropout
 from keras.layers import Flatten
 from keras.layers import MaxPooling2D
 from keras.models import Sequential
-import numpy as np
-import os
 from PIL import Image
 from PIL import ImageOps
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelBinarizer
+import numpy as np
+import os
+import pandas as pd
+
+from constants import MODEL_BASE_PATH
+from constants import DATASETS
+
+SIGN_TRAIN_PATH = r"E:/LakeheadU/Sign MNIST/sign_mnist_train.csv"
+SIGN_TEST_PATH = r"E:/LakeheadU/Sign MNIST/sign_mnist_test.csv"
 
 
 def main():
-    num_classes = 10
-    model_path = r"C:/Users/Manik/Desktop/my_aug_model"
+    args = docopt(__doc__)
+    dataset = args["--dataset"] or "MNIST"
+    if dataset not in DATASETS.keys():
+        print(f"    [INFO]\t\"{dataset} not in the list of datasets trained on.\"")
+        return
 
+    if dataset == "MNIST":
+        num_classes = 10
+        model_select = "my_aug_model"
+    else:
+        num_classes = 24
+        model_select = "my_sign_model"
+
+    model_path = os.path.join(MODEL_BASE_PATH, model_select)
     _train = not os.path.isdir(model_path)
 
     if _train:
+        if dataset == "MNIST":
+            # the data, split between train and test sets
+            (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-        # the data, split between train and test sets
-        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+            # Scale images to the [0, 1] range
+            ltr = len(x_train)
+            for i in range(ltr):
+                if i % 1000 == 0:
+                    print(f"Processed {i} / {ltr}")
+                im = x_train[i]
+                im = np.array(ImageOps.invert(Image.fromarray(np.uint8(im))))
+                im = np.reshape(im, (1, 28, 28))
+                x_train = np.vstack((x_train, im))
+                y_train = np.append(y_train, y_train[i])
 
-        # Scale images to the [0, 1] range
-        ltr = len(x_train)
-        for i in range(ltr):
-            if i % 1000 == 0:
-                print(f"Processed {i} / {ltr}")
-            im = x_train[i]
-            im = np.array(ImageOps.invert(Image.fromarray(np.uint8(im))))
-            im = np.reshape(im, (1, 28, 28))
-            x_train = np.vstack((x_train, im))
-            y_train = np.append(y_train, y_train[i])
+            lts = len(x_test)
+            for i in range(lts):
+                if i % 1000 == 0:
+                    print(f"Processed {i} / {lts}")
+                im = x_test[i]
+                im = np.array(ImageOps.invert(Image.fromarray(np.uint8(im))))
+                im = np.reshape(im, (1, 28, 28))
+                x_test = np.vstack((x_test, im))
+                y_test = np.append(y_test, y_test[i])
 
-        lts = len(x_test)
-        for i in range(lts):
-            if i % 1000 == 0:
-                print(f"Processed {i} / {lts}")
-            im = x_test[i]
-            im = np.array(ImageOps.invert(Image.fromarray(np.uint8(im))))
-            im = np.reshape(im, (1, 28, 28))
-            x_test = np.vstack((x_test, im))
-            y_test = np.append(y_test, y_test[i])
+        else:
+            train_df = pd.read_csv(SIGN_TRAIN_PATH)
+            test_df = pd.read_csv(SIGN_TEST_PATH)
+
+            train_images = train_df.loc[:, 'pixel1':].to_numpy()
+            train_labels = train_df['label'].to_numpy()
+
+            test_images = test_df.loc[:, 'pixel1':].to_numpy()
+            test_labels = test_df['label'].to_numpy()
+
+            x_train = np.array([], dtype=int)
+            y_train = np.array([], dtype=int)
+            for i in range(len(train_images)):
+                if (i + 1) % 1000 == 0:
+                    print(f"Processed {i + 1} / {len(train_images)} images")
+                img = train_images[i]
+                img = np.reshape(img, (28, 28))
+                img_inv = np.array(ImageOps.invert(Image.fromarray(np.uint8(img))))
+                img = np.reshape(img, (1, 28, 28))
+                img_inv = np.reshape(img_inv, (1, 28, 28))
+                if i == 0:
+                    x_train = np.vstack((img, img_inv))
+                else:
+                    x_train = np.vstack((x_train, img, img_inv))
+                label = train_labels[i]
+                if label >= 10:
+                    label -= 1
+                y_train = np.append(np.append(y_train, label), label)
+
+            x_test = np.array([], dtype=int)
+            y_test = np.array([], dtype=int)
+            for i in range(len(test_images)):
+                if (i + 1) % 1000 == 0:
+                    print(f"\rProcessed {i + 1} / {len(test_images)} images")
+                img = test_images[i]
+                img = np.reshape(img, (28, 28))
+                img_inv = np.array(ImageOps.invert(Image.fromarray(np.uint8(img))))
+                img = np.reshape(img, (1, 28, 28))
+                img_inv = np.reshape(img_inv, (1, 28, 28))
+                if i == 0:
+                    x_test = np.vstack((img, img_inv))
+                else:
+                    x_test = np.vstack((x_test, img, img_inv))
+                label = test_labels[i]
+                if label >= 10:
+                    label -= 1
+                y_test = np.append(np.append(y_test, label), label)
 
         x_train = x_train.astype("float32") / 255
         x_test = x_test.astype("float32") / 255
+
         # Make sure images have shape (28, 28, 1)
         x_train = np.expand_dims(x_train, -1)
         x_test = np.expand_dims(x_test, -1)
