aff7a2b3902b75acda4e464ff3851cdb582539c7
manikd31 <manik.dhingra311@gmail.com>
2021-11-03 13:37:41 -0400

    add constants for test-videos

--- constants.py
+++ constants.py
@@ -1,12 +1,14 @@
 # Directories to use as default path-in and path-out
 MODEL_BASE_PATH = r"C:/Users/Manik/Desktop"
-TEST_PATH_IN = r"C:/Users/Manik/Desktop/data/copy-data"
-TEST_PATH_OUT = r"C:/Users/Manik/Desktop/data/preprocess"
+TEST_PATH_IN = r"C:/Users/Manik/Desktop/data/Cleaned Dataset"
+TEST_PATH_OUT = r"E:/LakeheadU/Final Project Data/data"
+TEST_PATH_TEST_VIDEOS = r"E:/LakeheadU/Final Project Data/data/test_videos"
 TEST_FILE_NAME = "user02.mp4"
 TEST_TARGET_FPS = 4
-TEST_TARGET_FRAMES = 40
-TEST_RESIZE_FRAME_WIDTH = 224
-TEST_RESIZE_FRAME_HEIGHT = 224
+TEST_TARGET_FRAMES = 20
+TEST_RESIZE_FRAME_WIDTH = 100
+TEST_RESIZE_FRAME_HEIGHT = 100
+TEST_NUM_TEST_VIDEOS_PER_CLASS = 10
 
 # Video extension
 VIDEO_EXT = ".mp4"
